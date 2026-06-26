611c0754d9b188b9a4ecb34bbccaeac3a8dcc1f3
Mustafa Akagündüz <akagunduzmustafa00@gmail.com>
2026-06-26 11:12:56 +0300

    footer removal

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -8,7 +8,7 @@ export function Footer() {
     return (
         <footer className="py-6 bg-background border-t border-border">
             <div className="container mx-auto px-4 md:px-6 text-center text-muted-foreground">
-                <p>&copy; {new Date().getFullYear()} Mustafa Akagündüz. {translations["footer.rights"]}</p>
+                {/* <p>&copy; {new Date().getFullYear()} Mustafa Akagündüz. {translations["footer.rights"]}</p> */}
             </div>
         </footer>
     );
